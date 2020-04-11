6167df4e14c4febfd4ab34879220be173f10fa38
mohsen <moh.motamede@gmail.com>
2020-04-11 17:12:34 +0430

    linked-list: remove and search implimented

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -1,31 +1,53 @@
 const readline = require('readline');
-// const process = require('process');
-
-const createNode = (value, next = null) => {
-    return {
-        value,
-        next
-    };
-}
-
-// const addNodeIntoLastNode = (value, lastNode) => {
-//     const newNode = createNode(value);
-//     lastNode.next = newNode;
-//     return lastNode
-// };
 
 class LinkedList {
     constructor(value) {
         if (value) {
-            this.lastNode = createNode(value);
+            this.lastNode = this.createNode(value);
             this.lastNode.next = null;
             this.head = this.lastNode;
         }
     }
     addNode(value) {
-        const newNode = createNode(value);
+        const newNode = this.createNode(value);
         this.lastNode.next = newNode;
         this.lastNode = newNode;
+    }
+
+
+    createNode(value, next = null) {
+        return {
+            value: `${value}`,
+            next
+        };
+    }
+    search(value) {
+        const findList = []
+        let node = this.head;
+        do {
+            if (node.value.includes(value)) {
+                findList.push(node)
+            }
+            node = node.next;
+        } while (node && node.value);
+        return findList;
+    }
+
+    remove(value) {
+        let node = this.head;
+        let prevNode = null;
+        do {
+            if (node.value == value) {
+                if (!prevNode) {
+                    console.log("you have not permission for remove head")
+                } else {
+                    prevNode.next = node.next;
+                    return;
+                }
+            }
+            prevNode = node
+            node = node.next;
+        } while (node && node.value);
     }
 }
 
@@ -38,10 +60,24 @@ const rl = readline.createInterface({
 let linkedList;
 rl.prompt()
 rl.on('line', (value) => {
-    if (!linkedList) {
-        linkedList = new LinkedList(value);
-    } else {
-        linkedList.addNode(value);
+    if (Number.isInteger(+value)) {
+        if (!linkedList) {
+            linkedList = new LinkedList(value);
+        } else {
+            linkedList.addNode(value);
+        }
+    } else if (value.startsWith('s')) {
+        const searchValue = value.split(' ')[1];
+        const nodes = linkedList.search(searchValue);
+        if (Array.isArray(nodes)) {
+            console.log(`search result: [${nodes.map(n => n.value).join(', ')}]`)
+        } else {
+            console.log(`no result`);
+        }
+    } else if (value.startsWith('r')) {
+        const searchValue = value.split(' ')[1];
+        linkedList.remove(searchValue);
+        console.log('remove complit')
     }
 });
 rl.prompt();
